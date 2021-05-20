e6064cf8c8463b840c9b8e013d14d46ed08a6c3e
hendryadytia <f3.money@gmail.com>
2021-05-20 20:41:21 +0700

    update file sql

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 17 Apr 2021 pada 20.08
+-- Waktu pembuatan: 20 Bulan Mei 2021 pada 15.05
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.2.34
 
@@ -38,113 +38,40 @@ CREATE TABLE `akses` (
 --
 
 INSERT INTO `akses` (`id_akses`, `nama_akses`, `deskripsi`) VALUES
-(1, 'administrator', 'sebagai  pengelola kendali penuh pada sistem aplikasi'),
-(2, 'Asisten admin', 'sebagai pengelola sistem stok barang, penjualan dan laporan'),
-(3, 'Editor', 'Editor adalah Administrator yang dapat mengedit records');
+(1, 'administrator', 'sebagai  pengelola kendali penuh pada sistem inventaris'),
+(2, 'staff', 'sebagai staf input inventaris');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `bank`
+-- Struktur dari tabel `baranginv`
 --
 
-CREATE TABLE `bank` (
-  `id` int(11) NOT NULL,
-  `nama_bank` varchar(10) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `bank`
---
-
-INSERT INTO `bank` (`id`, `nama_bank`) VALUES
-(1, 'MANDIRI'),
-(2, 'BNI'),
-(3, 'BCA'),
-(4, 'BRI'),
-(5, 'CIMB Niaga');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `barang`
---
-
-CREATE TABLE `barang` (
-  `id_barang` int(11) NOT NULL,
+CREATE TABLE `baranginv` (
+  `id_barang` int(5) NOT NULL,
   `nama_barang` varchar(30) NOT NULL,
-  `id_kategori` int(11) NOT NULL,
-  `ukuran` varchar(5) NOT NULL,
-  `harga` int(20) NOT NULL,
-  `foto` varchar(80) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `no_invn` varchar(15) DEFAULT NULL,
+  `pemilik` varchar(15) NOT NULL,
+  `id_kerusakan` int(1) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `tgl_keluar` date NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data untuk tabel `baranginv`
 --
 
-INSERT INTO `barang` (`id_barang`, `nama_barang`, `id_kategori`, `ukuran`, `harga`, `foto`) VALUES
-(102, 'Cutting Sticker Putih Doff', 26, ' 6', 25, '04ae22fb4ea05629b7f826734406763b.jpg'),
-(103, 'Jasa Pasang Sticker', 28, ' 6', 50000, '87832d0983b0bbe95114a3515a192471.jpg'),
-(104, 'Sticker Oracal Merah', 26, ' 6', 25, 'fed42346c6dbd0f892bccfeca8a18aab.jpg'),
-(105, 'Stiker Oracal 2 warna ', 26, ' 6', 50, 'c196d884ce0e5417b791b1f60ec480cf.jpg'),
-(106, 'Akrilik Laser 15x51cm 3mm', 22, ' 6', 120000, 'f6fd9d74c2083550c1af790601b5c5d3.jpg');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `detail_pembelian`
---
-
-CREATE TABLE `detail_pembelian` (
-  `idp` int(11) NOT NULL,
-  `jenis_trans` int(11) NOT NULL,
-  `grand_total` bigint(20) NOT NULL,
-  `catatan` varchar(300) NOT NULL,
-  `tgl_trf` date NOT NULL,
-  `operator` varchar(30) NOT NULL,
-  `id_pembayaran` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `detail_penjualan`
---
-
-CREATE TABLE `detail_penjualan` (
-  `id` int(11) NOT NULL,
-  `no_trf` varchar(50) NOT NULL,
-  `nama_pelanggan` varchar(35) NOT NULL,
-  `totalpure` bigint(20) NOT NULL,
-  `grand_total` bigint(20) NOT NULL,
-  `diskon` int(3) NOT NULL,
-  `bayar` bigint(20) NOT NULL,
-  `kembalian` bigint(20) NOT NULL,
-  `catatan` varchar(50) NOT NULL,
-  `tgl_trf` date NOT NULL,
-  `jam_trf` time NOT NULL,
-  `id_pembayaran` int(2) NOT NULL,
-  `no_rek` int(18) DEFAULT NULL,
-  `atas_nama` varchar(35) NOT NULL,
-  `id_bank` int(2) DEFAULT NULL,
-  `operator` varchar(30) NOT NULL,
-  `nomor_telp` varchar(15) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `kas_masuk`
---
-
-CREATE TABLE `kas_masuk` (
-  `idmsk` int(30) NOT NULL,
-  `tgl_msk` date NOT NULL,
-  `catatan` text NOT NULL,
-  `jenis` int(2) NOT NULL,
-  `jumlah` int(30) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+INSERT INTO `baranginv` (`id_barang`, `nama_barang`, `no_invn`, `pemilik`, `id_kerusakan`, `tgl_masuk`, `tgl_keluar`, `status`) VALUES
+(4, 'kolostrus216', 'aa423152bcd', 'erwany', 30, '2021-05-02', '2021-05-15', 1),
+(5, 'kamikaze008901', 'afaik45653', 'gondrong', 30, '2021-05-16', '0000-00-00', 0),
+(6, 'ewfsdfa', '32rwrq23', 'aaada', 29, '2021-04-26', '2021-05-07', 1),
+(7, 'ingco1210', 'ing44542', 'iksan', 31, '2021-05-16', '2021-05-18', 1),
+(8, 'hikers221', '123124kkas', 'ghozi', 30, '2021-05-16', '2021-05-31', 1),
+(9, 'jinka13123', 'askdfjhuuaac', 'uwada', 30, '2021-05-19', '2021-05-13', 1),
+(10, 'kowalski2009', 'af2safaw3fa', 'iknssasf', 29, '2021-05-20', '0000-00-00', 0),
+(11, 'kolostrus215weq', 'aa423152bcdqweq', 'gondrongew', 29, '2021-05-26', '0000-00-00', 0),
+(12, 'kolostrus2160999', 'ijinfain', 'sfjoia', 29, '2021-05-04', '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -162,12 +89,10 @@ CREATE TABLE `kategori` (
 --
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
-(22, 'Akrilik'),
-(23, 'Besi'),
-(24, 'Kertas'),
-(26, 'Sticker'),
-(27, 'Neon Box'),
-(28, 'jasa');
+(32, 'Fatal'),
+(31, 'Berat'),
+(30, 'Sedang'),
+(29, 'Ringan');
 
 -- --------------------------------------------------------
 
@@ -190,135 +115,8 @@ CREATE TABLE `operator` (
 --
 
 INSERT INTO `operator` (`id_operator`, `nama_operator`, `username`, `password`, `id_akses`, `last_login`, `foto`) VALUES
-(9, 'Adit', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, '2021-04-18', 'avatar3.png'),
-(15, 'kasir', 'kasir', 'c7911af3adbd12a035b289556d96470a', 2, '2020-12-26', 'ebb1798a06597d68f86106e4c76183bb.png'),
-(19, 'Editor', 'editor', '5aee9dbd2a188839105073571bee1b1f', 3, '2021-01-02', 'd9192320c0b35f7f3d5d107728c70042.png');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pembayaran`
---
-
-CREATE TABLE `pembayaran` (
-  `id_byr` int(2) NOT NULL,
-  `metode` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `pembayaran`
---
-
-INSERT INTO `pembayaran` (`id_byr`, `metode`) VALUES
-(1, 'Cash'),
-(2, 'Transfer');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `penjualan`
---
-
-CREATE TABLE `penjualan` (
-  `id_transaksi` int(11) NOT NULL,
-  `id_dtlpen` int(5) NOT NULL,
-  `id_barang` int(11) NOT NULL,
-  `pjgb` varchar(100) NOT NULL,
-  `jumlah_stok` int(11) NOT NULL,
-  `harga_barang` bigint(20) NOT NULL,
-  `sub_total` bigint(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `stok`
---
-
-CREATE TABLE `stok` (
-  `id_stok` int(11) NOT NULL,
-  `id_barang` int(11) NOT NULL,
-  `stok_barang` int(11) NOT NULL,
-  `tanggal_stok` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `stok`
---
-
-INSERT INTO `stok` (`id_stok`, `id_barang`, `stok_barang`, `tanggal_stok`) VALUES
-(59, 103, 197, '2020-12-02'),
-(60, 76, 99916, '2021-02-20'),
-(67, 83, 9996, '2021-01-21'),
-(68, 84, 998, '2021-01-09'),
-(64, 80, 9996, '2021-02-04'),
-(65, 81, 990, '2021-02-26'),
-(66, 82, 986, '2021-02-26'),
-(69, 85, 998, '2021-01-27'),
-(70, 86, 998, '2021-01-21'),
-(71, 87, 99, '2021-01-11'),
-(72, 88, 96, '2021-02-06'),
-(73, 89, 106, '2021-01-27'),
-(74, 91, 100, '2021-01-21'),
-(75, 90, 97, '2021-01-27'),
-(76, 92, 94, '2021-01-27'),
-(77, 93, 14, '2021-01-21'),
-(78, 94, 295, '2021-01-28'),
-(79, 95, 299, '2021-01-26'),
-(80, 96, 99, '2021-01-26'),
-(81, 97, 967, '2021-02-26'),
-(83, 99, 88, '2021-02-26'),
-(84, 100, 999, '2021-02-17'),
-(85, 101, 999, '2021-02-17'),
-(86, 102, 100, '2021-02-19'),
-(87, 103, 998, '2021-02-20'),
-(88, 104, 999, '2021-02-27'),
-(89, 105, 98, '2021-02-27'),
-(90, 106, 9, '2021-03-01');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `transaksi_kas`
---
-
-CREATE TABLE `transaksi_kas` (
-  `id` int(2) NOT NULL,
-  `jenis` varchar(30) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `transaksi_kas`
---
-
-INSERT INTO `transaksi_kas` (`id`, `jenis`) VALUES
-(1, 'Pembelian Perlengkapan'),
-(2, 'Pembelian Peralatan'),
-(3, 'lain-lain'),
-(4, 'kasbon');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `ukuran`
---
-
-CREATE TABLE `ukuran` (
-  `id_ukuran` int(11) NOT NULL,
-  `nama_ukuran` varchar(35) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `ukuran`
---
-
-INSERT INTO `ukuran` (`id_ukuran`, `nama_ukuran`) VALUES
-(1, 'S'),
-(2, 'M'),
-(3, 'L'),
-(4, 'XL'),
-(5, 'XXL'),
-(6, 'No Size');
+(9, 'Adit', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, '2021-05-20', 'avatar3.png'),
+(15, 'kasir', 'kasir', 'c7911af3adbd12a035b289556d96470a', 2, '2021-05-16', 'ebb1798a06597d68f86106e4c76183bb.png');
 
 --
 -- Indexes for dumped tables
@@ -331,34 +129,10 @@ ALTER TABLE `akses`
   ADD PRIMARY KEY (`id_akses`);
 
 --
--- Indeks untuk tabel `bank`
+-- Indeks untuk tabel `baranginv`
 --
-ALTER TABLE `bank`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `barang`
---
-ALTER TABLE `barang`
+ALTER TABLE `baranginv`
   ADD PRIMARY KEY (`id_barang`);
-
---
--- Indeks untuk tabel `detail_pembelian`
---
-ALTER TABLE `detail_pembelian`
-  ADD PRIMARY KEY (`idp`);
-
---
--- Indeks untuk tabel `detail_penjualan`
---
-ALTER TABLE `detail_penjualan`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `kas_masuk`
---
-ALTER TABLE `kas_masuk`
-  ADD PRIMARY KEY (`idmsk`);
 
 --
 -- Indeks untuk tabel `kategori`
@@ -373,36 +147,6 @@ ALTER TABLE `operator`
   ADD PRIMARY KEY (`id_operator`);
 
 --
--- Indeks untuk tabel `pembayaran`
---
-ALTER TABLE `pembayaran`
-  ADD PRIMARY KEY (`id_byr`);
-
---
--- Indeks untuk tabel `penjualan`
---
-ALTER TABLE `penjualan`
-  ADD PRIMARY KEY (`id_transaksi`);
-
---
--- Indeks untuk tabel `stok`
---
-ALTER TABLE `stok`
-  ADD PRIMARY KEY (`id_stok`);
-
---
--- Indeks untuk tabel `transaksi_kas`
---
-ALTER TABLE `transaksi_kas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks untuk tabel `ukuran`
---
-ALTER TABLE `ukuran`
-  ADD PRIMARY KEY (`id_ukuran`);
-
---
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -413,76 +157,22 @@ ALTER TABLE `akses`
   MODIFY `id_akses` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `bank`
+-- AUTO_INCREMENT untuk tabel `baranginv`
 --
-ALTER TABLE `bank`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT untuk tabel `barang`
---
-ALTER TABLE `barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
-
---
--- AUTO_INCREMENT untuk tabel `detail_pembelian`
---
-ALTER TABLE `detail_pembelian`
-  MODIFY `idp` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `detail_penjualan`
---
-ALTER TABLE `detail_penjualan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `kas_masuk`
---
-ALTER TABLE `kas_masuk`
-  MODIFY `idmsk` int(30) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `baranginv`
+  MODIFY `id_barang` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT untuk tabel `operator`
 --
 ALTER TABLE `operator`
   MODIFY `id_operator` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
-
---
--- AUTO_INCREMENT untuk tabel `pembayaran`
---
-ALTER TABLE `pembayaran`
-  MODIFY `id_byr` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `penjualan`
---
-ALTER TABLE `penjualan`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `stok`
---
-ALTER TABLE `stok`
-  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
-
---
--- AUTO_INCREMENT untuk tabel `transaksi_kas`
---
-ALTER TABLE `transaksi_kas`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT untuk tabel `ukuran`
---
-ALTER TABLE `ukuran`
-  MODIFY `id_ukuran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
